cab1981413529374bed0217df3391a4a7a49a45f
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-03-23 08:29:28 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Matrici6
+{
+    internal class Program
+    {
+        static int[,] stampa(int[,] matrice)
+        {
+            int a = 100, b = 10;
+
+            for ( int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for(int j = 0;  j < matrice.GetLength(1); j++)
+                {
+                    matrice[i, j] = a;
+                    a = b - i;
+                    Console.Write(matrice[i, j] + "\t");
+                }
+                Console.WriteLine();
+                b = 10;
+                a = a - b;
+            }
+            return matrice;
+        }
+        static void Main(string[] args)
+        {
+            int[,] M = new int[10, 10];
+
+            stampa(M);
+        }
+    }
+}
